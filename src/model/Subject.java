77f54d56cdddd992200d9ac57781c3173401890f
giorgizena123 <zenaishvili621@gmail.com>
2025-05-14 01:25:35 +0400

    Update Subject.java

--- src/model/Subject.java
+++ src/model/Subject.java
@@ -1,4 +1,44 @@
-package model;
+package com.yourcompany.school.model;
 
 public class Subject {
+    private int id;
+    private String subjectName;
+
+
+    public Subject() {
+    }
+
+    public Subject(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public Subject(int id, String subjectName) {
+        this.id = id;
+        this.subjectName = subjectName;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "id=" + id +
+                ", subjectName='" + subjectName + '\'' +
+                '}';
+    }
 }
