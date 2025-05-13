4cc5f30d1b6a7389c91df8cbad7a096073ab5abd
giorgizena123 <zenaishvili621@gmail.com>
2025-05-14 01:24:16 +0400

    Update StudentDao.java

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -1,4 +1,85 @@
-package dao;
+
+package com.yourcompany.school.dao;
+
+import com.yourcompany.school.database.DatabaseConnection;
+import com.yourcompany.school.model.Student;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentDao {
+
+    public void create(Student student) throws SQLException {
+        String sql = "INSERT INTO students (first_name, last_name, grade) VALUES (?, ?, ?)";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, student.getFirstName());
+            preparedStatement.setString(2, student.getLastName());
+            preparedStatement.setInt(3, student.getGrade());
+            preparedStatement.executeUpdate();
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                student.setId(generatedKeys.getInt(1));
+            }
+        }
+    }
+
+    public Student read(int id) throws SQLException {
+        String sql = "SELECT id, first_name, last_name, grade FROM students WHERE id = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Student(
+                        resultSet.getInt("id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getInt("grade")
+                );
+            }
+            return null;
+        }
+    }
+
+    public List<Student> readAll() throws SQLException {
+        String sql = "SELECT id, first_name, last_name, grade FROM students";
+        List<Student> students = new ArrayList<>();
+        try (Connection connection = DatabaseConnection.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                students.add(new Student(
+                        resultSet.getInt("id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getInt("grade")
+                ));
+            }
+            return students;
+        }
+    }
+
+    public void update(Student student) throws SQLException {
+        String sql = "UPDATE students SET first_name = ?, last_name = ?, grade = ? WHERE id = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, student.getFirstName());
+            preparedStatement.setString(2, student.getLastName());
+            preparedStatement.setInt(3, student.getGrade());
+            preparedStatement.setInt(4, student.getId());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM students WHERE id = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        }
+    }
 }
