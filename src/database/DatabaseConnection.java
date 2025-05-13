45dded8b1a1629d29ea37aa89496ad847ccb533d
giorgizena123 <zenaishvili621@gmail.com>
2025-05-14 01:25:05 +0400

    Update DatabaseConnection.java

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -1,4 +1,20 @@
-package database;
+package com.yourcompany.school.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/school";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL JDBC driver not found!", e);
+        }
+    }
 }
