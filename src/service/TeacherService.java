be67a94b01671907e7aa5aad86c356051ed2c94e
giorgizena123 <zenaishvili621@gmail.com>
2025-05-14 01:26:14 +0400

    Update TeacherService.java

--- src/service/TeacherService.java
+++ src/service/TeacherService.java
@@ -1,4 +1,32 @@
-package service;
+package com.yourcompany.school.service;
+
+import com.yourcompany.school.dao.SubjectDao;
+import com.yourcompany.school.dao.TeacherDao;
+import com.yourcompany.school.model.Subject;
+import com.yourcompany.school.model.Teacher;
+
+import java.sql.SQLException;
 
 public class TeacherService {
+    private final TeacherDao teacherDao;
+    private final SubjectDao subjectDao;
+
+    public TeacherService() {
+        this.teacherDao = new TeacherDao();
+        this.subjectDao = new SubjectDao();
+    }
+
+    public String getSubjectNameByTeacherId(int teacherId) throws SQLException {
+        Teacher teacher = teacherDao.read(teacherId);
+        if (teacher != null) {
+            Subject subject = subjectDao.read(teacher.getSubjectId());
+            if (subject != null) {
+                return subject.getSubjectName();
+            } else {
+                return "საგანი ვერ მოიძებნა მასწავლებლისთვის ID: " + teacherId;
+            }
+        } else {
+            return "მასწავლებელი ვერ მოიძებნა ID: " + teacherId;
+        }
+    }
 }
