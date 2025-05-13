c8df839f3b190438e6d54395d039decfb9c3d943
giorgizena123 <zenaishvili621@gmail.com>
2025-05-14 01:25:21 +0400

    Update Student.java

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,68 @@
-package model;
+package com.yourcompany.school.model;
 
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int grade;
+
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, int grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+    public Student(int id, String firstName, String lastName, int grade) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
